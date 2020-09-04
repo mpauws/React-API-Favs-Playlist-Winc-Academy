a1ece85ef937fcf790568bf135e56d78ddd5c19f
Claudia Guldenstern <claudiaguldenstern@Marcos-MBP-2.fritz.box>
2020-09-04 10:20:00 +0200

    minor changes

--- my-app/src/components/SongOverview.js
+++ my-app/src/components/SongOverview.js
@@ -5,80 +5,79 @@ import SongListItem from "./SongListItem";
 import allSongsArray from "./raw-data/songs.js";
 
 function SongOverview() {
-   const [songs, setSongs] = useState(allSongsArray);
-   const [sorting, setSorting] = useState("");
+  const [songs, setSongs] = useState(allSongsArray);
+  const [sorting, setSorting] = useState("");
 
-   // Add Song
-   const addSongToList = (item) => {
-      item.id = songs.length + 1;
-      if (item.songtitle === "") {
-         item.songtitle = "Unknown Songtitle";
-      }
-      if (item.artist === "") {
-         item.artist = "Unknown Artist";
-      }
-      setSongs([...songs, item]);
-   };
+  // Add Song
+  const addSongToList = (item) => {
+    item.id = songs.length + 1;
+    if (item.songtitle === "") {
+      item.songtitle = "Unknown Songtitle";
+    }
+    if (item.artist === "") {
+      item.artist = "Unknown Artist";
+    }
+    setSongs([...songs, item]);
+  };
 
-   // Remove Song
-   const handleRemoveItem = (e) => {
-      const songItem = e.target.id;
-      setSongs(songs.filter((item) => item.id !== songItem));
-   };
+  // Remove Song
+  const handleRemoveItem = (e) => {
+    const songItem = e.target.id;
+    setSongs(songs.filter((item) => item.id !== songItem));
+  };
 
-   // Sort Songs
-   const onChangeSort = (sortMethod) => {
-      setSorting(sortMethod);
-   };
+  // Sort Songs
+  const onChangeSort = (sortMethod) => {
+    setSorting(sortMethod);
+  };
 
-   if (sorting === "songtitle-Z-A") {
-      songs.sort((a, b) => (a.songtitle < b.songtitle ? 1 : -1));
-   }
-   if (sorting === "songtitle-A-Z") {
-      songs.sort((a, b) => (a.songtitle > b.songtitle ? 1 : -1));
-   }
-   if (sorting === "artist-A-Z") {
-      songs.sort((a, b) => (a.artist > b.artist ? 1 : -1));
-   }
-   if (sorting === "artist-Z-A") {
-      songs.sort((a, b) => (a.artist < b.artist ? 1 : -1));
-   }
-   if (sorting === "genre-A-Z") {
-      songs.sort((a, b) => (a.genre > b.genre ? 1 : -1));
-   }
-   if (sorting === "genre-Z-A") {
-      songs.sort((a, b) => (a.genre < b.genre ? 1 : -1));
-   }
-   if (sorting === "1star-5star") {
-      songs.sort((a, b) => (a.rating > b.rating ? 1 : -1));
-   }
-   if (sorting === "5star-1star") {
-      songs.sort((a, b) => (a.rating < b.rating ? 1 : -1));
-   }
+  if (sorting === "songtitle-Z-A") {
+    songs.sort((a, b) => (a.songtitle < b.songtitle ? 1 : -1));
+  }
+  if (sorting === "songtitle-A-Z") {
+    songs.sort((a, b) => (a.songtitle > b.songtitle ? 1 : -1));
+  }
+  if (sorting === "artist-A-Z") {
+    songs.sort((a, b) => (a.artist > b.artist ? 1 : -1));
+  }
+  if (sorting === "artist-Z-A") {
+    songs.sort((a, b) => (a.artist < b.artist ? 1 : -1));
+  }
+  if (sorting === "genre-A-Z") {
+    songs.sort((a, b) => (a.genre > b.genre ? 1 : -1));
+  }
+  if (sorting === "genre-Z-A") {
+    songs.sort((a, b) => (a.genre < b.genre ? 1 : -1));
+  }
+  if (sorting === "1star-5star") {
+    songs.sort((a, b) => (a.rating > b.rating ? 1 : -1));
+  }
+  if (sorting === "5star-1star") {
+    songs.sort((a, b) => (a.rating < b.rating ? 1 : -1));
+  }
 
-   return (
-      <div>
-         <SongForm addSongToList={addSongToList} />
-         <h2>Overview Favorite Songs</h2>
-         <table>
-            <SongOverviewHeader onChangeSort={onChangeSort} />
-            <tbody>
-               {songs
-                  ? songs.map((song) => (
-                       <SongListItem
-                          id={song.id}
-                          songtitle={song.songtitle}
-                          artist={song.artist}
-                          genre={song.genre}
-                          rating={song.rating}
-                          handleRemoveItem={handleRemoveItem}
-                       />
-                    ))
-                  : null}
-            </tbody>
-         </table>
-      </div>
-   );
+  return (
+    <div>
+      <SongForm addSongToList={addSongToList} />
+      <h2>Overview Favorite Songs</h2>
+      <table>
+        <SongOverviewHeader onChangeSort={onChangeSort} />
+        <tbody>
+          {songs &&
+            songs.map((song) => (
+              <SongListItem
+                id={song.id}
+                songtitle={song.songtitle}
+                artist={song.artist}
+                genre={song.genre}
+                rating={song.rating}
+                handleRemoveItem={handleRemoveItem}
+              />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default SongOverview;
